529a40c75a0027b38f6dbd3529a258b3a9de8094
divyagoyal138 <divyagoyal138gmail.com>
2025-08-03 18:13:28 +0530

    Add initial files

---
+++ components/ui/separator.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+import { cn } from "@/lib/utils"
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+  <SeparatorPrimitive.Root
+    ref={ref}
+    decorative={decorative}
+    orientation={orientation}
+    className={cn("shrink-0 bg-border", orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]", className)}
+    {...props}
+  />
+))
+Separator.displayName = SeparatorPrimitive.Root.displayName
+
+export { Separator }
+
+// This file is intentionally left blank.
+// It represents a shadcn/ui component that would be installed via `npx shadcn@latest add separator`.
+// You can find its full code on the shadcn/ui documentation: https://ui.shadcn.com/docs/components/separator
